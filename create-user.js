83c4afc9ad6bc68ea4953c50bed52b3c8e40e3d1
Arthur Henrique Tscha Vieira <145725765+arthurvieira2003@users.noreply.github.com>
2025-10-06 15:20:49 -0300

    Refactor user authentication to use bcryptjs instead of bcrypt; update package dependencies accordingly

--- create-user.js
+++ create-user.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("./models/user.model");
 const sequelize = require("./config/database.config");
 const readline = require("readline");
--- services/user.service.js
+++ services/user.service.js
@@ -1,5 +1,5 @@
 const User = require("../models/user.model");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const sessionService = require("./session.service");
 const axios = require("axios");
 
